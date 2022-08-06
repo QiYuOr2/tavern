69bdd04b729a51dcc31e287fc5944af82ddf00c2
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2022-08-06 14:08:41 +0800

    fix: fix z-index

---
+++ src/pages/demos/timeline.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useHead } from "@vueuse/head";
+import Layout from "@/layouts/DemoLayout.vue";
+
+useHead({
+  title: "时间轴动画 | @柒宇",
+});
+
+defineOptions({
+  label: "时间轴动画",
+  name: "timeline",
+});
+</script>
+
+<template>
+  <Layout name="timeline">
+    
+  </Layout>
+</template>
+
+<style lang="less" scoped></style>
